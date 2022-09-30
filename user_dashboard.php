159c42404e54398a74132cb784974860c0de98dd
pianop477 <108887102+pianop477@users.noreply.github.com>
2022-10-01 02:24:59 +0300

    Update user_dashboard.php

--- user_dashboard.php
+++ user_dashboard.php
@@ -25,11 +25,12 @@ if(empty($_SESSION['user_id'])) {
 <!--section-->
 
 <div>
-    <div>
-        <h2 class="text-center page-header" style="margin-top: auto;">Your Customer Dashboard,  <?php echo $_SESSION['fullname']; ?></h2>
+    <div class="container-fluid;" style="position: absolute; width: 100%; margin-top: 8%;">
+        <h2 class="text-center page-header" style="margin-top: auto;">Your Customer Dashboard,  
+        <span style="color: blue;"><?php echo $_SESSION['fullname']; ?></span></h2>
         <div class="container-fluid" style="width: 80%; display: flex; justify-content: space-between; align-items: center;">
             <div class="container-fluid" style="width: 38.3%; background: orange; border-radius: 8px;">
-                <h2 class="text-center page-header">MY ORDERS</h2>
+                <h2 class="text-center page-header">MY CART</h2>
                 <p>
                 <?php 
                 $total_select = mysqli_query($conn, "SELECT * FROM orders WHERE id = '$logged_user' ");
@@ -40,8 +41,9 @@ if(empty($_SESSION['user_id'])) {
                 }
                 ?>
                 </p>
+                <p style=" text-align: center; font-weight:bold; "><a href="customertable.php">View Cart 	<i class="fa fa-arrow-circle-right fa-2x"></i></a></p>
             </div>
-            <div class="container-fluid" style="width: 38.3%; background: lightgreen; border-radius: 8px;">
+            <div class="container-fluid" style="width: 38.3%; background: lightgreen; border-radius: 8px; color: black;">
                 <h2 class="text-center page-header">ALL MENU</h2>
                 <p><?php
                 $total_select = mysqli_query($conn, "SELECT * FROM menus");
@@ -51,6 +53,7 @@ if(empty($_SESSION['user_id'])) {
                     echo '<h3 style="font-size: 4rem; text-align: center;">0</h3>';
                 }
                 ?></p>
+                <p style=" text-align: center; font-weight:bold; "><a href="index.php">View Menu 	<i class="fa fa-arrow-circle-right fa-2x"></i></a></p>
             </div>
         </div>
     
